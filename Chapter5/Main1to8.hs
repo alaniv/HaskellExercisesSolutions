bd92c29e439cc375c6f391a560dc3c31ca732aa8
alaniv <alaniv@users.noreply.github.com>
2020-11-14 21:22:48 -0300

    Update and rename Main1to6.hs to Main1to8.hs

--- Chapter5/Main1to8.hs
+++ Chapter5/Main1to8.hs
@@ -24,6 +24,20 @@ divisors n = [ x | x <- [1..n], n `mod` x == 0]
 perfects :: Int -> [Int]
 perfects n = [ x | x <- [1..n] , sum (divisors x) == 2*x ]
 
+--ex7
+pairs :: [(Int,Int)]
+pairs = concat [ [(x,y) | y <- [3,4] ] | x <- [1,2] ]
+
+--ex8
+find :: Eq a => a -> [(a,b)] -> [b]
+find k t = [ v | (k',v) <- t, k == k']
+
+positions :: Eq a => a -> [a] -> [Int]
+positions x xs = [ i |  (x', i) <- zip xs [0..], x == x']
+
+positions2 :: Eq a => a -> [a] -> [Int]
+positions2 x xs = find x (zip xs [0..])
+
 main :: IO ()
 main = do
     print(sumSquares)
@@ -32,3 +46,5 @@ main = do
     print(replicate2 3 True)
     print(pyths 10)
     print(perfects 496)
+    print(pairs)
+    print(positions2 1 [1,2,3,4,1,2,3,4])
