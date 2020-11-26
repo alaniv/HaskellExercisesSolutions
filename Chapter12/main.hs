16f5ac14910859d439da1175b85ffa3a5ec2ccdc
alaniv <alaniv@users.noreply.github.com>
2020-11-25 23:02:24 -0300

    Update main.hs

--- Chapter12/main.hs
+++ Chapter12/main.hs
@@ -47,6 +47,31 @@ instance Monad ((->) c) where
     (fca >>= g) xc = g (fca xc) xc
 -}
 
+-- ex7
+data Expr a = Var a | Val Int | Add (Expr a) (Expr a)
+    deriving Show
+
+instance Functor Expr where
+    -- fmap :: (a -> b) -> Expr a -> Expr b
+    fmap f (Var xa) = Var (f xa)
+    fmap _ (Val n) = Val n
+    fmap f (Add e1 e2) = Add (fmap f e1) (fmap f e2)
+
+instance Applicative Expr where
+    -- pure :: a -> Expr a
+    pure x = Var x
+    
+    -- (<*>) :: (Expr (a -> b)) -> (Expr a) -> (Expr b)
+    (Var f) <*> xa = fmap f xa
+    (Val n) <*> _ = Val n
+    (Add f1 f2) <*> xa = Add (f1 <*> xa) (f2 <*> xa)
+
+instance Monad Expr where
+    -- (>>=) :: (Expr a) -> (a -> Expr b) -> (Expr b)
+    (Var xa) >>= f = f xa
+    (Val n) >>= _ = (Val n)
+    (Add e1 e2) >>= f = (Add (e1 >>= f) (e2 >>= f))
+
 main :: IO ()
 main = do
     print $ fmap (+1) (Node Leaf 22 Leaf)
